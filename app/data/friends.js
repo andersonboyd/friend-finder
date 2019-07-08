118560c46903a5b48834e67cbf7522cd88ed2691
Anderson <andersonswaimboyd@gmail.com>
2019-07-08 18:53:58 -0500

    debugging modal photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendArray = [
     {
         name: "This guy!",
-        photo: "/images/thisguy.jpg",
+        photo: "https://assets3.thrillist.com/v1/image/1299823/size/tmg-article_default_mobile.jpg",
         scores: [5, 4, 3, 5, 1, 3, 2, 5, 4, 4]
     },
     {
         name: "That guy!",
-        photo: "/images/thatguy.jpg",
+        photo: "https://birdinflight.imgix.net/wp-content/uploads/2016/12/vr-headphones-stock-photo_cover.jpg?fm=pjpg&q=80&fit=crop&crop=faces&w=630&h=331",
         scores: [5, 4, 3, 2, 1, 2, 3, 4, 5, 1]
     },
     {
         name: "This grandma!",
-        photo: "/images/thuglady.jpeg",
+        photo: "https://media0ch-a.akamaihd.net/25/20/95cdb962ac8de7f47912ca54fc4bd2a3.jpeg",
         scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5]
     },
     {
         name: "Harold!",
-        photo: "/images/harold.jpg",
+        photo: "https://i.pinimg.com/originals/4d/b7/b7/4db7b7ecb39c4eebc5b8f5358773e4a2.jpg",
         scores: [1, 2, 1, 2, 3, 2, 1, 2, 3, 4]
     },
     {
         name: "This guy with too much pasta! Maybe you two can share it?",
-        photo: "/images/pastaguy.jpg",
+        photo: "http://i.imgur.com/y5uqJgV.jpg",
         scores: [3, 4, 3, 3, 4, 2, 1, 5, 5, 5]
     }
 ];
