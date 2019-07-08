e464c44a1ee6414b3e8b9def1837c85f0916be11
Anderson <andersonswaimboyd@gmail.com>
2019-07-08 18:18:46 -0500

    debugging images in friend array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 var friendArray = [
     {
         name: "This guy!",
-        photo: "../images/thisguy.jpg",
+        photo: "/images/thisguy.jpg",
         scores: [5, 4, 3, 5, 1, 3, 2, 5, 4, 4]
     },
     {
         name: "That guy!",
-        photo: "../images/thatguy.jpg",
+        photo: "/images/thatguy.jpg",
         scores: [5, 4, 3, 2, 1, 2, 3, 4, 5, 1]
     },
     {
         name: "This grandma!",
-        photo: "../images/thuglady.jpeg",
+        photo: "/images/thuglady.jpeg",
         scores: [1, 2, 3, 4, 5, 4, 3, 2, 1, 5]
     },
     {
         name: "Harold!",
-        photo: "../images/harold.jpg",
+        photo: "/images/harold.jpg",
         scores: [1, 2, 1, 2, 3, 2, 1, 2, 3, 4]
     },
     {
         name: "This guy with too much pasta! Maybe you two can share it?",
-        photo: "../images/pastaguy.jpg",
+        photo: "/images/pastaguy.jpg",
         scores: [3, 4, 3, 3, 4, 2, 1, 5, 5, 5]
     }
 ];
